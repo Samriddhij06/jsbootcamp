08fc33d5a89f9b94238e8c48d2d5c380f854ff97
Samriddhij06 <samriddhi.jain@freightdeck.com>
2022-01-25 13:05:04 +0530

    second commit

---
+++ module1/arrays.js
@@ -0,0 +1,8 @@
+var things = ['laptop','speaker',13]
+things.length; 
+
+//add an item to the array
+things.push(17);
+//splice(starting from index n remove x items)
+//add additional arguments to the array
+things.splice(1,0,'pen','paper');
